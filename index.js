75285aa8179adf6500002fca2b11e80074f21ac7
Shlok Natarajan <shlok.natarajan@gmail.com>
2024-05-29 13:28:44 -0700

    feat: simplified ga 2

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 let clickCounter = 0;
 function handleClick() {
+    console.log("click handled");
+    console.log(`Click Counter: ${clickCounter}`);
     clickCounter += 1;
-    gtag('event', 'click_event_from_personal', {
-        'app_name': 'testing-google-analytics',
-        'screen_name': 'Personal POC',
-        'click_counter': this.clickCounter
-      });
+    gtag('event','button_click');
 }
