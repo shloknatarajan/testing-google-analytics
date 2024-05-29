d6c7d9375838d91fac5fd86c71bc9397721f7d49
Shlok Natarajan <shlok.natarajan@gmail.com>
2024-05-29 13:29:36 -0700

    fix: added index.js to bottom

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let clickCounter = 0;
 function handleClick() {
-    console.log("click handled");
-    console.log(`Click Counter: ${clickCounter}`);
+    console.log("click handled")
     clickCounter += 1;
-    gtag('event','button_click');
+    gtag('event', 'click_event_from_personal', {
+        'app_name': 'testing-google-analytics',
+        'screen_name': 'Personal POC',
+        'click_counter': this.clickCounter
+      });
 }
